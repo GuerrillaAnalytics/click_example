3889dc4dab84baf363a558b1d17837b53c03f167
Enda Ridge <guerrillaanalytics@gmail.com>
2017-09-02 15:31:27 +0100

    Renamed main method to be closer to Click examples

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,21 +9,21 @@ def runner():
 
 
 def test_cli(runner):
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.cli)
     assert result.exit_code == 0
     assert not result.exception
     assert result.output.strip() == 'Hello, world.'
 
 
 def test_cli_with_option(runner):
-    result = runner.invoke(cli.main, ['--as-cowboy'])
+    result = runner.invoke(cli.cli, ['--as-cowboy'])
     assert not result.exception
     assert result.exit_code == 0
     assert result.output.strip() == 'Howdy, world.'
 
 
 def test_cli_with_arg(runner):
-    result = runner.invoke(cli.main, ['Enda'])
+    result = runner.invoke(cli.cli, ['Enda'])
     assert result.exit_code == 0
     assert not result.exception
     assert result.output.strip() == 'Hello, Enda.'
