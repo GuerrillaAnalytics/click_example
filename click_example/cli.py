258528ae45a082fb058958d9200560c5d6e9262b
Enda Ridge <guerrillaanalytics@gmail.com>
2017-09-03 09:22:20 +0100

    Fixed import errors. But that breaks PEP so added rule exception to tox.ini

--- click_example/cli.py
+++ click_example/cli.py
@@ -1,12 +1,12 @@
 import click
 import sys
 import os.path
-from click_example.commands.cloudflare_grp.cmd import cloudflare
-from click_example.commands.uptime_grp.cmd import uptimerobot
 
 # Based on https://stackoverflow.com/questions/34643620/
 # how-can-i-split-my-click-commands-each-with-a-set-of-sub-commands-into-multipl
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from click_example.commands.cloudflare_grp.cmd import cloudflare
+from click_example.commands.uptime_grp.cmd import uptimerobot
 
 
 @click.group()
