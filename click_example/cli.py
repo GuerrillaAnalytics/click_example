1bf14003f7a6f5495e11f4ede6036d95a6566064
Enda Ridge <guerrillaanalytics@gmail.com>
2017-09-03 08:29:32 +0100

    included example question from stack overflow

--- click_example/cli.py
+++ click_example/cli.py
@@ -1,14 +1,79 @@
 import click
 
 
-@click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
+@click.group()
 @click.version_option()
-def cli(name, as_cowboy):
-    """A working example of nested Python Click commands"""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+def cli():
+    pass
+
+
+@cli.group()
+@click.pass_context
+def cloudflare(ctx):
+    pass
+
+
+@cloudflare.group('zone')
+def cloudflare_zone():
+    pass
+
+
+@cloudflare_zone.command('add')
+@click.option('--jumpstart', '-j', default=True)
+@click.option('--organization', '-o', default='')
+@click.argument('url')
+@click.pass_obj
+def cloudflare_zone_add(ctx, url, jumpstart, organization):
+    pass
+
+
+@cloudflare.group('record')
+def cloudflare_record():
+    pass
+
+
+@cloudflare_record.command('add')
+@click.option('--ttl', '-t')
+@click.argument('domain')
+@click.argument('name')
+@click.argument('type')
+@click.argument('content')
+@click.pass_obj
+def cloudflare_record_add(ctx, domain, name, type, content, ttl):
+    pass
+
+
+@cloudflare_record.command('edit')
+@click.option('--ttl', '-t')
+@click.argument('domain')
+@click.argument('name')
+@click.argument('type')
+@click.argument('content')
+@click.pass_obj
+def cloudflare_record_edit(ctx, domain):
+    pass
+
+
+# uptime part
+@cli.group()
+@click.pass_context
+def uptimerobot(ctx):
+    pass
+
+
+@uptimerobot.command('add')
+@click.option('--alert', '-a', default=True)
+@click.argument('name')
+@click.argument('url')
+@click.pass_obj
+def uptimerobot_add(ctx, name, url, alert):
+    pass
+
+
+@uptimerobot.command('delete')
+@click.argument('names', nargs=-1, required=True)
+@click.pass_obj
+def uptimerobot_delete(ctx, names):
     pass
 
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,18 +12,19 @@ def test_cli(runner):
     result = runner.invoke(cli.cli)
     assert result.exit_code == 0
     assert not result.exception
-    assert result.output.strip() == 'Hello, world.'
-
-
-def test_cli_with_option(runner):
-    result = runner.invoke(cli.cli, ['--as-cowboy'])
-    assert not result.exception
-    assert result.exit_code == 0
-    assert result.output.strip() == 'Howdy, world.'
-
-
-def test_cli_with_arg(runner):
-    result = runner.invoke(cli.cli, ['Enda'])
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.output.strip() == 'Hello, Enda.'
+    # assert result.output.strip() == 'Hello, world.'
+    print(result.output.strip())
+#
+#
+# def test_cli_with_option(runner):
+#     result = runner.invoke(cli.cli, ['--as-cowboy'])
+#     assert not result.exception
+#     assert result.exit_code == 0
+#     assert result.output.strip() == 'Howdy, world.'
+#
+#
+# def test_cli_with_arg(runner):
+#     result = runner.invoke(cli.cli, ['Enda'])
+#     assert result.exit_code == 0
+#     assert not result.exception
+#     assert result.output.strip() == 'Hello, Enda.'
