e2e821485de616343adb53484447a9593e4c1435
Enda Ridge <guerrillaanalytics@gmail.com>
2017-09-03 11:15:48 +0100

    cleaned up code and added a py35 environment

--- click_example/cli.py
+++ click_example/cli.py
@@ -1,9 +1,14 @@
+"""
+Based on
+https://stackoverflow.com/questions/34643620/
+how-can-i-split-my-click-commands-each-with-a-set-of-sub-commands-into-multipl
+
+"""
 import click
 import sys
 import os.path
 
-# Based on https://stackoverflow.com/questions/34643620/
-# how-can-i-split-my-click-commands-each-with-a-set-of-sub-commands-into-multipl
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from click_example.commands.cloudflare_grp.cmd import cloudflare
 from click_example.commands.uptime_grp.cmd import uptimerobot
