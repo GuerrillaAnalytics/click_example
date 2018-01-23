be15079908d0a592266f657bee98522983f00250
GuerrillaAnalytics <enda.ridge@gmail.com>
2018-01-23 20:27:49 +0000

    renamed second command group

--- click_example/cli.py
+++ click_example/cli.py
@@ -11,7 +11,7 @@ import os.path
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from click_example.commands.command1_grp.cmd import command1
-from click_example.commands.uptime_grp.cmd import uptimerobot
+from click_example.commands.command2_grp.cmd import command2
 
 
 @click.group()
@@ -21,7 +21,7 @@ def cli():
 
 
 cli.add_command(command1)
-cli.add_command(uptimerobot)
+cli.add_command(command2)
 
 
 if __name__ == '__main__':
