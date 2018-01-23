f8c313ae3499e97706a9ddd17ccc8488fae32384
GuerrillaAnalytics <enda.ridge@gmail.com>
2018-01-23 20:16:46 +0000

    minor edit

--- click_example/cli.py
+++ click_example/cli.py
@@ -10,7 +10,7 @@ import os.path
 
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from click_example.commands.cloudflare_grp.cmd import cloudflare
+from click_example.commands.command1_grp.cmd import cloudflare
 from click_example.commands.uptime_grp.cmd import uptimerobot
 
 
